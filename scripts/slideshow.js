80fee6fd31045173f7ba9709254e178f835261a6
rygo0712 <ryan_jefferson_go@dlsu.edu.ph>
2022-04-29 20:07:23 +0800

    var change

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,15 +1,15 @@
-let i = 0;
+let i_slideshow = 0;
 let str = null;
 const timer = ms => new Promise(res => setTimeout(res, ms))
 
 async function slideShow() {
 	while(true){
-		str = "url("  + "images/" + i.toString() + ".jpg)";
+		str = "url("  + "images/" + i_slideshow.toString() + ".jpg)";
 		document.getElementById("slideshow").style.backgroundImage = str;
 		if(i < 3)
-			i++;
+			i_slideshow++;
 		else
-			i = 0;
+			i_slideshow = 0;
 		await timer(8000);
 	}
 }
