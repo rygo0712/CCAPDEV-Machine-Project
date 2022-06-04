1a26a5b5798e1fadf881b4e0abd268b1404d3dfd
rygo0712 <ryan_jefferson_go@dlsu.edu.ph>
2022-06-04 15:31:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = new express();
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(express.static(__dirname + '/'));
+/***************** Where we will use mongoose */
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/galleryDB',
+{useNewURLParser: true, useUnifiedTopology: true}); // Create database connection
+
+// For File Uploads
+const fileUpload = require('express-fileupload');
+
+// Post initializations. 1, the schema model, the path directory for file uploads, and a static resource folder //
+const path = require('path'); // Local path directory for our static resource folder
+
+// Initialize data and static folder that our app will use
+app.use(express.json()); // Use JSON throughout our app for parsing
+app.use(express.urlencoded( {extended: true})); // Information consists of more than just strings
+app.use(express.static('public')); // static directory name, meaning that the application will also refer to a folder named 'public'
+app.use(fileUpload()); // for fileuploading
+
+/* using handlebars */
+var hbs = require('hbs');
+app.set('view engine','hbs');
 
 app.get('/', function(req,res)
 {
-    res.sendFile(__dirname + '\\' + 'home.html');
+    res.sendFile(__dirname + '\\' + 'index.html');
 });
 
 
