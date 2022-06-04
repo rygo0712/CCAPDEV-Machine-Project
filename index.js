23d97bc1d9e83f111805bbaef2772959c79ba7d1
rygo0712 <ryan_jefferson_go@dlsu.edu.ph>
2022-06-04 15:23:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static(__dirname + '/'));
 
 app.get('/', function(req,res)
