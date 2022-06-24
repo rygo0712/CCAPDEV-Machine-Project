3ce0004f24efe43188ddf95e6231b9a700bfc6bf
rygo0712 <ryan_jefferson_go@dlsu.edu.ph>
2022-06-24 18:49:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.use(session({
     store: MongoStore.create({ mongoUrl: mongoURI }),
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24 * 7 }
+    cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24 * 14 }
   }));
 
 /** Setting server */
